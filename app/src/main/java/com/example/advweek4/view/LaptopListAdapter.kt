98f5109cbc107aa11fd3d21f080db9d16841aea0
michaeldsnto <michaeldesanto80@gmail.com>
2023-10-05 23:48:25 +0700

    Connect to laptop.json

--- app/src/main/java/com/example/advweek4/view/LaptopListAdapter.kt
+++ app/src/main/java/com/example/advweek4/view/LaptopListAdapter.kt
@@ -3,22 +3,27 @@ package com.example.advweek4.view
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.net.toUri
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.advweek4.R
 import com.example.advweek4.model.Laptop
 import com.example.advweek4.model.Student
+import com.squareup.picasso.Picasso
 
 class LaptopListAdapter(val laptopList:ArrayList<Laptop>) :RecyclerView.Adapter<LaptopListAdapter.LaptopViewHolder>(){
     class LaptopViewHolder(view: View):RecyclerView.ViewHolder(view){
         val txtBrand: TextView
         val txtModel: TextView
         val txtPrice: TextView
+        val photoLaptop: ImageView
         init {
             txtBrand = view.findViewById(R.id.txtBrand)
             txtModel = view.findViewById(R.id.txtModel)
             txtPrice = view.findViewById(R.id.txtPrice)
+            photoLaptop = view.findViewById(R.id.photoLaptop)
         }
     }
 
@@ -29,9 +34,11 @@ class LaptopListAdapter(val laptopList:ArrayList<Laptop>) :RecyclerView.Adapter<
     }
 
     override fun onBindViewHolder(holder: LaptopViewHolder, position: Int) {
-        holder.txtBrand.text = laptopList[position].brand
-        holder.txtModel.text = laptopList[position].model
-        holder.txtPrice.text = laptopList[position].price.toString()
+        holder.txtBrand.text = "Brand: " + laptopList[position].brand
+        holder.txtModel.text = "Model: " + laptopList[position].model
+        holder.txtPrice.text = "Price" + laptopList[position].price.toString()
+        val imageUrl = laptopList[position].photoLaptop
+        Picasso.get().load(imageUrl).resize(350, 300).into(holder.photoLaptop)
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/example/advweek4/viewmodel/LaptopViewModel.kt
+++ app/src/main/java/com/example/advweek4/viewmodel/LaptopViewModel.kt
@@ -1,20 +1,18 @@
 package com.example.advweek4.viewmodel
 
 import android.app.Application
-import android.content.Context
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import com.android.volley.DefaultRetryPolicy
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.advweek4.model.Laptop
-import com.example.advweek4.model.Student
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
-import java.util.*
+
 
 class LaptopViewModel(application: Application): AndroidViewModel(application) {
     val laptopLD = MutableLiveData<ArrayList<Laptop>>()
@@ -29,7 +27,7 @@ class LaptopViewModel(application: Application): AndroidViewModel(application) {
         loadingLD.value = true
 
         queue = Volley.newRequestQueue(getApplication())
-        val url = "http://10.0.2.2/laptop/laptop.json"
+        val url = "http://10.0.2.2:8007/laptop/laptop.json"
 
         val stringRequest = StringRequest(
             Request.Method.GET, url,
@@ -45,9 +43,12 @@ class LaptopViewModel(application: Application): AndroidViewModel(application) {
                 laptopLoadErrorLD.value = true
                 loadingLD.value = false
             },
-
-            )
-
+        )
+        stringRequest.retryPolicy = DefaultRetryPolicy(
+            6000,
+            DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+            DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+        )
 
         stringRequest.setTag(TAG)
         queue?.add(stringRequest)
